5b7813ffd7595fcfe0a1d80878055b15517c5bd9
ankur <ankur.elex.bit@gmail.com>
2015-05-23 14:57:44 +0530

    Test library

---
+++ managerlib/src/main/java/com/practice/ankur/managerlib/MainLibClass.java
@@ -0,0 +1,20 @@
+package com.practice.ankur.managerlib;
+
+/**
+ * Created by ankur on 5/23/15.
+ */
+public abstract class MainLibClass {
+
+    int data;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    abstract public void print();
+
+}
