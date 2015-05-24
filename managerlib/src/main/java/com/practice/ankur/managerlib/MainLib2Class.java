fc75169c772c22465fd3f65ee024d47f6bc84160
ankur <ankur.elex.bit@gmail.com>
2015-05-24 18:18:41 +0530

    Test2

---
+++ managerlib/src/main/java/com/practice/ankur/managerlib/MainLib2Class.java
@@ -0,0 +1,17 @@
+package com.practice.ankur.managerlib;
+
+/**
+ * Created by ankur on 5/24/15.
+ */
+public class MainLib2Class {
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    private int data;
+}
